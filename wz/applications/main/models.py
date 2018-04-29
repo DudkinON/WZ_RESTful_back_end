da3a29d88efe344c0da00836ccb2f0063304c4c1
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-29 04:02:33 -0500

    feat: Add Message model

--- wz/applications/main/models.py
+++ wz/applications/main/models.py
@@ -42,3 +42,20 @@ class Pages(models.Model):
         verbose_name = _('Page')
         verbose_name_plural = _('Pages')
 
+
+class Message(models.Model):
+    username = models.CharField(_("username"), max_length=64)
+    title = models.CharField(_("title"), max_length=100, blank=True, null=True, default=None)
+    email = models.EmailField(_("email"), max_length=40)
+    text = models.CharField(_("text"), max_length=250, default=None)
+    date = models.DateTimeField(_("date"), auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'main_message'
+        verbose_name = _('Message')
+        verbose_name_plural = _('Messages')
