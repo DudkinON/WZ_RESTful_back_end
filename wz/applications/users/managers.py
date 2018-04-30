4e4ddd9ae82c64680ba249fe82e6c86e4900e25d
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-30 01:20:16 -0500

    feat: Add create_superuser method in UserManager class

--- wz/applications/users/managers.py
+++ wz/applications/users/managers.py
@@ -32,3 +32,10 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', False)
         return self._add_user(email, password, **extra_fields)
 
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._add_user(email, password, **extra_fields)
