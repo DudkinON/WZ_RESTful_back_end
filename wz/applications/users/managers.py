ffe7ba7a33337a3827d05ff26a49d2d1f16cdddf
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-30 01:10:47 -0500

    feat: Add _add_user method to UserManager class

--- wz/applications/users/managers.py
+++ wz/applications/users/managers.py
@@ -5,4 +5,13 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    pass
+    def _add_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
